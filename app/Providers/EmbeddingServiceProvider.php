9f3e13f86e88d29d9236db27b0d287f0559d62ae
fikrimastor <fikrimastor@gmail.com>
2026-01-15 22:02:20 +0800

    feat: update embedding when memory updated #65

--- app/Providers/EmbeddingServiceProvider.php
+++ app/Providers/EmbeddingServiceProvider.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Actions\Memory\UpdateMemoryAction;
 use App\Contracts\EmbeddingDriverInterface;
+use App\Contracts\UpdateMemoryContract;
 use App\Services\EmbeddingManager;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class EmbeddingServiceProvider extends ServiceProvider
+class EmbeddingServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     /**
      * Register any application services.
@@ -19,14 +22,13 @@ class EmbeddingServiceProvider extends ServiceProvider
             __DIR__.'/../../config/embedding.php', 'embedding'
         );
 
-        $this->app->singleton(EmbeddingManager::class, function ($app) {
-            return new EmbeddingManager($app);
-        });
+        $this->app->singleton(EmbeddingManager::class, fn ($app) => new EmbeddingManager($app));
 
         // Register the default driver
-        $this->app->singleton(EmbeddingDriverInterface::class, function ($app) {
-            return $app[EmbeddingManager::class]->driver();
-        });
+        $this->app->singleton(EmbeddingDriverInterface::class, fn ($app) => $app[EmbeddingManager::class]->driver());
+
+        // Additional Contract
+        $this->app->singleton(UpdateMemoryContract::class, UpdateMemoryAction::class);
     }
 
     /**
